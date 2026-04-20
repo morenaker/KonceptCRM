21e857d7c558b7c906483195f42d1d0374169f0a
mader@koncepthk.cz <More.naker1>
2026-04-20 10:29:29 +0200

    fix: cast user.role pro Vercel build

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -25,7 +25,7 @@ export const authOptions: NextAuthOptions = {
           id: user.id,
           email: user.email,
           name: user.name,
-          role: user.role,
+          role: user.role as "ADMIN" | "USER",
         };
       },
     }),
